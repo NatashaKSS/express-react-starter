31a0aeb8a019d03be0ec818a8d8d90e92508498a
NatashaKSS <xnagol@gmail.com>
2017-06-22 00:19:28 +0800

    Added client files

---
+++ src/client/index.jsx
@@ -0,0 +1,8 @@
+require("./stylesheets/main.scss");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
